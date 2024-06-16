07ce0277e563dfb903f587265cb5c31c17bf7222
Molnsc <159168620+Molnsc@users.noreply.github.com>
2024-06-15 17:39:21 -0700

    Create accounttyperedeem.js

---
+++ accounttyperedeem.js
@@ -0,0 +1,20 @@
+[
+    {
+        "Token": "uufk49304",
+        "ServerValues": {
+            "S1": "Premium Trial"
+        }
+    },
+    {
+        "Token": "4235",
+        "ServerValues": {
+            "S1": "Premium"
+        }
+    },
+    {
+        "Token": "123",
+        "ServerValues": {
+            "S1": "Reseller"
+        }
+    }
+]
